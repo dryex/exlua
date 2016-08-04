1dd14fb9dd463d6bba4be591cab23d3b945e2fd8
Arto Bendiken <arto@bendiken.net>
2016-08-04 18:48:42 +0200

    Added test cases for Lua.eval() and Lua.eval!().

--- test/lua_test.exs
+++ test/lua_test.exs
@@ -4,4 +4,23 @@ defmodule LuaTest do
   use ExUnit.Case, async: true
 
   doctest Lua
+
+  test "Lua.eval/2" do
+    assert Lua.eval(Lua.State.new, "return 6 * 7") == {:ok, [42.0]}
+    assert Lua.eval(Lua.State.new, "foobar") == {:error,
+      {:badmatch, {:error, [{1, :luerl_parse, 'illegal call'}], []}}}
+  end
+
+  test "Lua.eval!/2" do
+    assert Lua.eval!(Lua.State.new, "return 6 * 7") == [42.0]
+    assert_raise Lua.Error, fn -> Lua.eval!(Lua.State.new, "foobar") end
+  end
+
+  test "Lua.eval_file/2" do
+    #Lua.eval_file(Lua.State.new, "etc/examples/hello.lua") # FIXME: shebang
+  end
+
+  test "Lua.eval_file!/2" do
+    #Lua.eval_file!(Lua.State.new, "etc/examples/hello.lua") # FIXME: shebang
+  end
 end
