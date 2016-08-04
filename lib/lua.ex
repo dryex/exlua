f96e39c8562dd1cd43565037fb3444ad8c1b0148
Arto Bendiken <arto@bendiken.net>
2016-08-04 16:10:07 +0200

    Implemented Lua.eval() and Lua.eval!().

--- lib/lua.ex
+++ lib/lua.ex
@@ -1,4 +1,18 @@
 # This is free and unencumbered software released into the public domain.
 
 defmodule Lua do
+  alias Lua.{Error, State}
+
+  @doc "Evaluates a string of Lua code."
+  def eval(%State{luerl: state}, code) do
+    :luerl.eval(code, state)
+  end
+
+  @doc "Evaluates a string of Lua code."
+  def eval!(%State{luerl: state}, code) do
+    case :luerl.eval(code, state) do
+      {:ok, result}    -> result
+      {:error, reason} -> raise Error, reason: reason, message: "Lua.eval!/2 failed"
+    end
+  end
 end
