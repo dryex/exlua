d5a76f6623eee6e6086aa42edef921c2f7b39348
Arto Bendiken <arto@bendiken.net>
2016-08-04 20:09:49 +0200

    Defined Lua.Chunk.

---
+++ lib/lua/chunk.ex
@@ -0,0 +1,7 @@
+# This is free and unencumbered software released into the public domain.
+
+defmodule Lua.Chunk do
+  defstruct luerl: nil
+
+  @type t :: struct
+end
---
+++ test/lua_chunk_test.exs
@@ -0,0 +1,7 @@
+# This is free and unencumbered software released into the public domain.
+
+defmodule LuaChunkTest do
+  use ExUnit.Case, async: true
+
+  doctest Lua.Chunk
+end
