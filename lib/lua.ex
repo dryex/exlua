de88fb0717f6d99352075254f59753597ec254ff
Arto Bendiken <arto@bendiken.net>
2016-08-04 16:22:40 +0200

    Implemented Lua.eval_file() and Lua.eval_file!().

--- lib/lua.ex
+++ lib/lua.ex
@@ -3,16 +3,29 @@
 defmodule Lua do
   alias Lua.{Error, State}
 
-  @doc "Evaluates a string of Lua code."
+  @doc "Evaluates a Lua code snippet."
   def eval(%State{luerl: state}, code) do
     :luerl.eval(code, state)
   end
 
-  @doc "Evaluates a string of Lua code."
+  @doc "Evaluates a Lua code snippet."
   def eval!(%State{luerl: state}, code) do
     case :luerl.eval(code, state) do
       {:ok, result}    -> result
-      {:error, reason} -> raise Error, reason: reason, message: "Lua.eval!/2 failed"
+      {:error, reason} -> raise Error, reason: reason, message: inspect(reason)
+    end
+  end
+
+  @doc "Evaluates a Lua file."
+  def eval_file(%State{luerl: state}, path) do
+    :luerl.evalfile(path, state)
+  end
+
+  @doc "Evaluates a Lua file."
+  def eval_file!(%State{luerl: state}, path) do
+    case :luerl.evalfile(path, state) do
+      {:ok, result}    -> result
+      {:error, reason} -> raise Error, reason: reason, message: inspect(reason)
     end
   end
 end
